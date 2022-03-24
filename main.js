d8aa0ace1a1897213fdbd5815869534ff27e8e7e
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-24 16:50:16 +0000

    Si inizia

---
+++ main.js
@@ -0,0 +1,41 @@
+let list = [
+{
+    'name' : 'Angela Caroll',
+    'role' : 'Chief editor',
+    'img' : 'angela-caroll-chief-editor.jpg'
+},
+{
+    'name' : 'Angela Lopez',
+    'role': 'Social media manager',
+    'img': 'angela-lopez-social-media-manager.jpg',
+},
+{
+    'name' : 'Barbara Ramos',
+    'role': 'Graphic designer',
+    'img': 'barbara-ramos-graphic-designer.jpg',
+},
+{
+    'name' : 'Scott Estrada',
+    'role': 'developer',
+    'img': 'scott-estrada-developer.jpg',
+},
+{
+    'name' : 'Walter-Gordon',
+    'role': 'Office Manager',
+    'img': 'walter-gordon-office-manager.jpg',
+},
+{
+    'name' : 'Wayne Barnett',
+    'role': 'Founder & Ceo',
+    'img': 'wayne-barnett-founder-ceo.jpg',
+},
+]
+
+console.log(list)
+
+for ( i=0; i < list.length; i++){
+
+
+
+    
+}
